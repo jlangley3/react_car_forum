7cb91cfc948d4405ed47ffecc7e455fb9fdb905d
jlangley3 <jlangleyiii@aol.com>
2020-07-02 13:41:21 -0400

    last

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-// import logo from './logo.svg';
+import logo from '../logo.svg';
 import '../App.css';
 import {BrowserRouter as Router,Route} from 'react-router-dom';
 import NavBar from '../components/NavBar';
@@ -10,21 +10,21 @@ import About from '../components/About';
 
 function App() {
   return (
-    // <div className="App">
-    //   <header className="App-header">
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //     <p>
-    //       Edit <code>src/App.js</code> and save to reload.
-    //     </p>
-    //     <a
-    //       className="App-link"
-    //       href="https://reactjs.org"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       Learn React
-    //     </a>
-    //   </header>
+    <div className="App">
+      <header className="App-header">
+        {/* <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a> */}
+      </header>
       <Router>
       <div>
         <NavBar />
@@ -35,7 +35,7 @@ function App() {
         </div>
       
     </Router>
-    // </div>
+    </div>
   );
 }
 
